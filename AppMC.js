be6d74883061135f04ac9e146c0c4e66b788dd6e
João Ataíde <ataide1981@gmail.com>
2018-11-10 17:50:29 +0000

    Add files via upload

---
+++ AppMC.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="Scripts/Angular/angular-route.min.js" />
+
+
+var app = angular.module('AppMC', ['ngTouch',"ui.grid", "ngRoute" ])
+app.config(function ($routeProvider) {
+    $routeProvider
+       .when("/home", {
+           templateUrl: "Templates/home.html",
+           controller: "homeController"
+       })
+      .when("/CriarActividade", {
+          templateUrl: "Templates/CriarActividade.html",
+          controller: "CriarActController"
+      })
+        .when("/ListarActividadesPorData", {
+            templateUrl: "Templates/ListarActividadesPorData.html",
+            controller: "ListarActividadesController"
+        })
+     .when("/DetalhesActividades", {
+         templateUrl: "Templates/DetalhesActividades.html",
+         controller: "DetalhesController"
+     })
+
+});
+
+//Para Reverter ordem da Lista de Actividades
+//app.filter('reverse', function () {
+//    return function (items) {
+//        return items.slice().reverse();
+//    };
+//});
